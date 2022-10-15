36834fd4e06631d62e3ddb1f0348ffc6280f42d9
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-10-15 10:56:10 +0500

    Update ArrayDequeCreator.java

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,41 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
-    public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+    public  ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+
+
+        ArrayDeque<Integer>arrayDeque=new ArrayDeque<>();
+
+        arrayDeque.add(firstQueue.remove());
+        arrayDeque.add(firstQueue.remove());
+        arrayDeque.add(secondQueue.remove());
+        arrayDeque.add(secondQueue.remove());
+
+
+        for (int i = 0; i < arrayDeque.size(); i++) {
+            try {
+
+                firstQueue.add(arrayDeque.getLast());
+                arrayDeque.removeLast();
+                arrayDeque.add(firstQueue.remove());
+                arrayDeque.add(firstQueue.remove());
+
+                secondQueue.add(arrayDeque.getLast());
+                arrayDeque.removeLast();
+                arrayDeque.add(secondQueue.remove());
+                arrayDeque.add(secondQueue.remove());
+
+
+
+            }catch (Exception ex){
+                break;
+            }
+        }
+
+        return arrayDeque;
+
+
     }
 }
