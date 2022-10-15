ff179e1da82909f60103f036f024b844de7992a6
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-10-15 10:57:01 +0500

    Update DishOrderDeterminer.java

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,43 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
-    public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+
+
+    public  List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+
+        List<Integer> integers = new LinkedList<>();
+
+        Queue<Integer> res = new LinkedList<>();
+
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            integers.add(i);
+        }
+
+        int c = 0;
+
+        int i = -1;
+
+        while (!integers.isEmpty()) {
+
+            i++;
+            c++;
+
+            if (c % everyDishNumberToEat == 0) {
+
+                res.add(integers.get(i));
+                integers.remove(i);
+                c = 0;
+                i--;
+            }
+            
+            if (i == integers.size() - 1)
+                i = -1;
+        }
+
+
+        return (List<Integer>) res;
     }
 }
